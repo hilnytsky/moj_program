5fb3d0b40c57a5302de4ddf96bb044086f312975
Hlib Ilnytsky <glebfarmon@gmail.com>
2025-10-26 10:47:53 +0100

    Program kalkulator

---
+++ kalkulator.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    int a = 10;
+    int b = 5;
+
+    printf("a + b = %i\n", a + b);
+    printf("a - b = %i\n", a - b);
+    printf("a * b = %i\n", a * b);
+    printf("a / b = %i\n", a / b);
+}
